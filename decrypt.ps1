02629c13d2e46a43c3a9aea72088435cbfa30722
Isaac Beizsley <23095702+iBeizsley@users.noreply.github.com>
2023-10-20 15:33:08 +0100

    Fix subfolder decryption

--- decrypt.ps1
+++ decrypt.ps1
@@ -3,15 +3,16 @@ param(
     [Parameter(Mandatory = $false)][string]$keyFile
 );
 
-if (!(Test-Path -Path "~/Documents")) { 
-    New-Item -Path "~/Documents" -ItemType Directory;
-}
-if (!(Test-Path -Path "~/Documents/decrypted")) { 
-    New-Item -Path "~/Documents/decrypted" -ItemType Directory;
+$out = "$HOME/Documents/decrypted/$($inputFile.TrimStart('./').TrimEnd('.aes256cbc'))";
+$outDir = Split-Path $out -Parent;
+
+if (!(Test-Path -Path "$outDir")) { 
+    New-Item -Path "$outDir" -ItemType Directory;
 }
 
 if ($keyFile -ne "") {
-    openssl aes-256-cbc -kfile "$keyFile" -pbkdf2 -iter 1000000 -d -in "$inputFile" -out "$HOME/Documents/decrypted/$($inputFile.TrimStart('./').TrimEnd('.aes256cbc'))";
-} else {
-    openssl aes-256-cbc -pbkdf2 -iter 1000000 -d -in "$inputFile" -out "$HOME/Documents/decrypted/$($inputFile.TrimStart('./').replace('.aes256cbc', ''))";
+    openssl aes-256-cbc -kfile "$keyFile" -pbkdf2 -iter 1000000 -d -in "$inputFile" -out "$out";
+}
+else {
+    openssl aes-256-cbc -pbkdf2 -iter 1000000 -d -in "$inputFile" -out "$out";
 }
